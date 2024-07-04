ce617a14957ada97b574a4d3236873b991a6d90c
egamovj <egamovj90@gmail.com>
2024-07-04 17:23:45 +0500

    navbar structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Container, Row } from "reactstrap";
+
+import "./header.css";
+
+import UserIcon from "../../assets/images/user-icon.png";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper">
+            <div className="logo">
+              <p>
+                <span>E</span>-store
+              </p>
+            </div>
 
-export default Header
+            <div className="navigation">
+              <ul className="menu">
+                <li className="nav__item">
+                  <NavLink to="/home">Home</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="/shop">Shop</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="/cart">Cart</NavLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className="nav__icons">
+              <span className="fav__icon">
+                <i class="ri-heart-line"></i>
+              </span>
+              <span className="cart__icon">
+                <i class="ri-shopping-bag-line"></i>
+              </span>
+              <span>
+                <img src={UserIcon} alt="" />
+              </span>
+            </div>
+
+            <div className="mobile__menu">
+              <span><i class="ri-menu-line"></i></span>
+            </div>
+
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import "remixicon/fonts/remixicon.css";
+import "bootstrap/dist/css/bootstrap.css";
+
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
