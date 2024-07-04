c5ce11d990d867ca99d407501cebcf36f2fe1c95
egamovj <egamovj90@gmail.com>
2024-07-04 18:17:28 +0500

    navbar structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { Container, Row } from "reactstrap";
+import { motion } from "framer-motion";
 
 import "./header.css";
 
@@ -8,6 +9,21 @@ import UserIcon from "../../assets/images/user-icon.png";
 import Bag from "../../assets/shopping-bag-line.svg";
 import Heart from "../../assets/heart-line.svg";
 import Menu from "../../assets/menu-line.svg";
+
+const nav__links = [
+  {
+    path: "home",
+    display: "Home",
+  },
+  {
+    path: "shop",
+    display: "Shop",
+  },
+  {
+    path: "cart",
+    display: "Cart",
+  },
+];
 
 const Header = () => {
   return (
@@ -23,39 +39,38 @@ const Header = () => {
 
             <div className="navigation">
               <ul className="menu">
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/home">
-                    Home
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/shop">
-                    Shop
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink className="nav__item-link" to="/cart">
-                    Cart
-                  </NavLink>
-                </li>
+                {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      className={(navClass) =>
+                        navClass.isActive ? "nav__active" : "nav__item-link"
+                      }
+                      to={item.path}
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
               </ul>
             </div>
 
             <div className="nav__icons">
               <span className="fav__icon">
                 <img src={Heart} />
+                <span className="badge">1</span>
               </span>
               <span className="cart__icon">
                 <img src={Bag} />
+                <span className="badge">1</span>
               </span>
               <span>
-                <img src={UserIcon} alt="" />
+                <motion.img whileTap={{ scale: 1.2 }} src={UserIcon} alt="" />
               </span>
             </div>
 
             <div className="mobile__menu">
               <span>
-              <img src={Menu} alt="" />
+                <img src={Menu} alt="" />
               </span>
             </div>
           </div>
