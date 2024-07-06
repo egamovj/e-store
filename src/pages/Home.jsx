d6e9190e1d78df9dfa6e86d4995e8dbf9d120b00
egamovj <egamovj90@gmail.com>
2024-07-06 12:32:54 +0500

    limited offers section clock

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,9 @@ import heroImg from "../assets/images/hero-img.png";
 import Services from "../services/Services";
 import ProductsList from "../components/UI/ProductsList";
 import products from "../assets/data/product";
+import Clock from "../components/UI/Clock";
+
+import counterImg from "../assets/images/counter-timer-img.png";
 
 const Home = () => {
   const [trendingProducts, setTrendingProducts] = useState([]);
@@ -79,6 +82,28 @@ const Home = () => {
             </Col>
 
             <ProductsList data={bestSalesProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className="timer__count">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6 mb-2">Limited Offers</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+              </div>
+              <Clock />
+
+              <motion.button whileTap={{scale: 1.2}} className="buy__btn store__btn">
+                <Link to="/shop">Visit Store</Link>
+              </motion.button>
+            </Col>
+
+            <Col lg="6" md="6" className="text-end">
+              <img src={counterImg} alt="" />
+            </Col>
           </Row>
         </Container>
       </section>
